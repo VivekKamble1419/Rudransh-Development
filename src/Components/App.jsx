098102e99d939ce219a7ca57631460a7292da1de
vivekkamble1419 <mrvivekkamble8@gmail.com>
2024-07-05 00:09:54 +0530

    Starting to built own startup

---
+++ src/Components/App.jsx
@@ -0,0 +1,29 @@
+import '../App.css';
+
+function App() {
+  return (
+    <>
+      <header className="navbar">
+        <div className="navbar-logo">Vivek Tech Guide</div>
+        <nav className="navbar-links">
+          <a href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="#review">Review</a>
+          <a href="#projects">Projects</a>
+          <a href="#contact">Contact Us</a>
+        </nav>
+       
+      </header>
+
+   
+
+
+
+      <footer className="footer">
+        <p>&copy; 2024 Vivek Tech Guide. All rights reserved.</p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
